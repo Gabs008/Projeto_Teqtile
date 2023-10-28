9ff486bea38cecbacf7ea507af1cb7870d0c09a0
Gabs008 <101597088+Gabs008@users.noreply.github.com>
2023-10-28 11:50:56 -0300

    Add graphql server

---
+++ servidor_graphql/index.js
@@ -0,0 +1,20 @@
+const {ApolloServer, gql } = require('apollo-server');
+
+//Definir recursos 
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+
+`;
+//Definir retornos
+const resolvers = {
+    Query: {
+        hello: ( ) => 'Hello world!'
+    }
+};
+
+//servidor
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen().then(({ url }) => console.log(`Server rodando ${url}`));
